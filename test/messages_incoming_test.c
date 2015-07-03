9244ee23fad71b6c48fc5c9387a1cdf84d350488
seunggi.hong <seunggi.hong@samsung.com>
2015-07-03 10:16:30 +0900

    3.0 Migration
    Change-Id: I720304894e0c0c55ab1c27ec250a4853c05f40da

--- test/messages_incoming_test.c
+++ test/messages_incoming_test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dlog.h>
 
 #include <glib.h>
 
@@ -7,8 +8,8 @@
 
 #define ERROR_CHECK(ret) \
 	if (MESSAGES_ERROR_NONE != ret) { \
-		printf("%d: error, ret=%d \n", __LINE__, ret); \
-		exit(1); \
+		dlog_print(DLOG_DEBUG, "MESSAGE_TEST", "%d: error, ret=%d \n", __LINE__, ret); \
+		return(0); \
 	}
 
 static GMainLoop *mainloop;
@@ -29,51 +30,51 @@ void incoming_cb(messages_message_h msg, void *user_data)
 	int i, nAddr;
 	char *addr = NULL;
 	time_t time;
-	struct tm *ptm;
+	struct tm ptm;
 
-	printf("Incoming Message\n");
+	dlog_print(DLOG_DEBUG, "MESSAGE_TEST", "Incoming Message\n");
 
 	messages_get_address_count(msg, &nAddr);
 	for (i=0; i < nAddr; i++) {
 		ret = messages_get_address(msg, i, &addr, NULL);
 		if (MESSAGES_ERROR_NONE == ret) {
-			printf("Address[%d]: %s\n", i, addr);
+			dlog_print(DLOG_DEBUG, "MESSAGE_TEST", "Address[%d]: %s\n", i, addr);
 			free(addr);
 		}
 	}
 
 	ret = messages_get_text(msg, &text);
 	if (MESSAGES_ERROR_NONE == ret) {
-		printf("Text: %s\n", text);
+		dlog_print(DLOG_DEBUG, "MESSAGE_TEST", "Text: %s\n", text);
 		free(text);
 	} else {
-		printf("%d: error, ret=%d\n", __LINE__, ret);
+		dlog_print(DLOG_DEBUG, "MESSAGE_TEST", "%d: error, ret=%d\n", __LINE__, ret);
 	}
 
 	
 	messages_get_message_type(msg, &msgType);
 	switch(msgType) {
 	case MESSAGES_TYPE_SMS:
-		printf("Type: SMS\n");
+		dlog_print(DLOG_DEBUG, "MESSAGE_TEST", "Type: SMS\n");
 		break;
 	case MESSAGES_TYPE_MMS:
-		printf("Type: MMS\n");
+		dlog_print(DLOG_DEBUG, "MESSAGE_TEST", "Type: MMS\n");
 		break;
 	default:
-		printf("Type: Unknown\n");
+		dlog_print(DLOG_DEBUG, "MESSAGE_TEST", "Type: Unknown\n");
 		break;
 	}
 	
 	messages_get_time(msg, &time);
-	printf("Time: %d, %s", (int)time, ctime(&time));
+	char buf[50];
+	if (ctime_r(&time, buf))
+		dlog_print(DLOG_DEBUG, "MESSAGE_TEST", "Time: %d, %s", (int)time, ctime_r(&time, buf));
 
-        ptm = gmtime(&time);
-        printf("gmtime test: %d.%d.%d %d:%d:%d \n",
-                ptm->tm_year, ptm->tm_mon, ptm->tm_mday,
-                ptm->tm_hour, ptm->tm_min, ptm->tm_sec
-        );
-
-
+	gmtime_r(&time, &ptm);
+	dlog_print(DLOG_DEBUG, "MESSAGE_TEST", "gmtime test: %d.%d.%d %d:%d:%d \n",
+			ptm.tm_year, ptm.tm_mon, ptm.tm_mday,
+			ptm.tm_hour, ptm.tm_min, ptm.tm_sec
+	);
 }
 
 int main(int argc, char *argv[])
